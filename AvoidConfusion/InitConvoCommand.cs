31aff9a481d267f0b6c1802c700eeb43160a7f69
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-12-30 16:31:16 +0300

    Update InitConvoCommand.cs

--- AvoidConfusion/InitConvoCommand.cs
+++ AvoidConfusion/InitConvoCommand.cs
@@ -1,4 +1,5 @@
-﻿#region .NET
+#region Using Statements - Using Deyimleri
+#region .NET
 using System;
 using System.Json;
 using System.IO;
@@ -24,7 +25,10 @@ using DSharpPlus.EventArgs;
 using DSharpPlus.Net;
 using Newtonsoft.Json;
 using DSharpPlus.CommandsNext.Entities;
+using AvoidConfusion.Entities;
 #endregion
+#endregion
+
 
 namespace AvoidConfusion
 {
@@ -59,7 +63,7 @@ namespace AvoidConfusion
                 resultEmbed.Description = "Konuşma başlığı boş, 250 karakterden uzun veya 1 karakterden kısa.";
                 await context.RespondAsync(embed: resultEmbed.Build());
             }
-            else if ((conversationDescription is ""))
+            else if (conversationDescription is "")
             {
                 //An invalid value is given.
                 //Yine geçersiz bir değer girildi.
@@ -73,80 +77,27 @@ namespace AvoidConfusion
 #nullable enable
                 try
                 {
-                    //Connect to the database.
-                    //Veritabanına bağlanalım.
-                    var connectionString = "Data Source=MasherCoder;Initial Catalog=AvoidConfusion; Username=Bot";
-                    using SqlConnection connection = new(connectionString);
-                    await connection.OpenAsync();
-                    //Get the snowflake of the guild where the command was sent.
-                    //Komutun gönderildiği sunucunun kar tanesini alalım.
-                    var snowflakeCommandSenderGuild = new SqlBinary((byte[]?)(BitConverter.GetBytes(context.Guild.Id)));
-                    while (connection.State != ConnectionState.Open)
-                    {
-                        //Do nothing while the connection gets opened.
-                        //Bağlantı açılmadıkça bir şey yapma.
-                    }
-
-                    //Do something when the connection is open.
-                    //Bağlantı açılınca bir şeyler yap.
-                    var getSnowflakeCommand = connection.CreateCommand();
-                    //Get guild snowflakes from the database.
-                    //Veritabanından lonca kar tanelerini al.
-                    getSnowflakeCommand.CommandText = "SELECT [Snowflake] FROM [dbo].[Guilds]";
-                    getSnowflakeCommand.CommandTimeout = 10;
-                    getSnowflakeCommand.CommandType = CommandType.Text;
-                    var dbDataReader = await getSnowflakeCommand.ExecuteReaderAsync();
-                    //If the snowflake of this guild is not in the database, 
-                    //register the guild to the database (but not set the conversation supervisior role.).
-                    //Eğer bu sunucunun kar tanesi veritabanında yoksa, 
-                    //bu sunucuyu veritabanına kaydedelim (ama konuşma gözlemcisi rolünü ayarlamayalım.).
-                    bool guildRecorded = false;
-                    for (; dbDataReader.Read();)
-                    {
-                        if ((SqlBinary)(dbDataReader["[Snowflake]"]) == snowflakeCommandSenderGuild)
-                        {
-                            //The guild is recorded to our database...
-                            //Sunucu veritabanımıza kayıtlıymış...
-                            guildRecorded = true;
-                        }
-                    }
-
-                    if (!guildRecorded)
-                    {
-                        //Record the guild to the database.
-                        //Sunucuyu veritabanına kaydet.
-                        var recordGuildCommand = connection.CreateCommand();
-                        var everyoneRole = new SqlBinary(BitConverter.GetBytes(context.Guild.EveryoneRole.Id));
-                        recordGuildCommand.CommandType = CommandType.Text;
-                        recordGuildCommand.CommandText = $"INSERT INTO [dbo].[Guilds] ([Snowflake], [ConversationSupervisiorRoleSnowflake]) VALUES (@GuildSnowflake,@ConvoSnowflake)";
-                        recordGuildCommand.Parameters.AddWithValue("@GuildSnowflake", snowflakeCommandSenderGuild);
-                        recordGuildCommand.Parameters.AddWithValue("@ConvoSnowflake", everyoneRole);
-                        await recordGuildCommand.ExecuteNonQueryAsync();
-
-                    }
+                    
                     //Init a conversation channel.
                     //Bir konuşma kanalı açalım.
                     var convoChannel = await context.Guild.CreateChannelAsync
                         (
-                            $"convo{((DateTime.Now.Ticks << 3) | (DateTime.Now.Ticks >> 61)):X10}",
+                            name:$"convo{((DateTime.Now.Ticks << 3) | (DateTime.Now.Ticks >> 61)):X8}",
+
                             ChannelType.Text,
                             topic: new Optional<string>(conversationDescription)
                         );
                     //Create a role for the conversation.
                     //Konuşma için bir rol yaratalım.
+                    var conversatingRole = await context.Guild.CreateRoleAsync(name:"Conversator",mentionable:(bool?) false,permissions:Permissions.None);
+                    
+                    await convoChannel.AddOverwriteAsync(conversatingRole,allow:
+                    (Permissions.SendMessages|Permissions.SendTtsMessages|Permissions.AttachFiles|Permissions.AccessChannels));
 
+                    await convoChannel.AddOverwriteAsync(context.Guild.EveryoneRole, deny:(Permissions.SendMessages|Permissions.SendTtsMessages|Permissions.AttachFiles|Permissions.AccessChannels));
 
-                    //Record the conversation channel to the channel.
-                    //Sunucuya konuşma kanalını kaydedelim.
-                    var getGuildIDCommand = connection.CreateCommand();
-                    getGuildIDCommand.CommandType = CommandType.Text;
-                    getGuildIDCommand.CommandText = "SELECT [Id] FROM [dbo].[Guilds] WHERE [Snowflake] = @GuildSnowflake";
-                    getGuildIDCommand.Parameters.AddWithValue("@GuildSnowflake", snowflakeCommandSenderGuild);
-                    await getGuildIDCommand.ExecuteScalarAsync();
-
-                    var recordChannelCommand = connection.CreateCommand();
-                    recordChannelCommand.CommandType = CommandType.Text;
-                    recordChannelCommand.CommandText = "INSERT INTO [dbo].[Conversations] ([GuildID], [ChannelSnowflake], [ConversatingUserRoleSnowflake],) VALUES ()";
+                    await ((DiscordMember)context.Message.Author).GrantRoleAsync(conversatingRole);
+                    
 
 #nullable disable
                 }
